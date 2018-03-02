019e86a3889f8255be4c3141d860a13a20ab2961
Elisa Urda <urda.elisa@gmail.com>
2018-03-02 16:56:50 -0500

    created files, started html, psuedocode, and js

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+// create objects including each question and all answer options
+
+var q1 = {
+    question: "Question 1",
+    a1: "a",
+    a2: "b",
+    a3: "c",
+    correct: "correct",
+}
+
+var q2 = {
+    question: "Question 2",
+    a1: "a",
+    a2: "b",
+    a3: "c",
+    correct: "correct",
+}
+
+var q3 = {
+    question: "Question 3",
+    a1: "a",
+    a2: "b",
+    a3: "c",
+    correct: "correct",
+}
+// create array of each object
+
+// when play game is clicked, game begins
+
+// question is chosen based on math.random for length of array
+
+// begin timer counting down from 30 seconds
+
+// when player clicks on an answer stop the time and switch to the next screen which shows the correct answer and the image
+
+// if time runs out before player clicks on an answer then switch to the next screen showing the correct answer and image
+
+// if correct add one to total correct answers
+
+// if incorrect or time runs out add one to total incorrect answers
+
+// after showing correct answer for 5 seconds switch to the next question
