55d2de1af0b42f27f8829cada0f3d68b8754bbbd
Elisa Urda <urda.elisa@gmail.com>
2018-03-02 19:49:45 -0500

    Started js, added some css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,42 @@
-// create objects including each question and all answer options
+// create an object for each question, properties include question, an array of all possible answers, and correct answer
 
 var q1 = {
     question: "Question 1",
-    a1: "a",
-    a2: "b",
-    a3: "c",
+    answers: ["a", "b", "c", "correct"],
     correct: "correct",
 }
 
 var q2 = {
     question: "Question 2",
-    a1: "a",
-    a2: "b",
-    a3: "c",
+    answers: ["a", "b", "c", "correct"],
     correct: "correct",
 }
 
 var q3 = {
     question: "Question 3",
-    a1: "a",
-    a2: "b",
-    a3: "c",
+    answers: ["a", "b", "c", "correct"],
     correct: "correct",
 }
-// create array of each object
+
+// create array containing all objects  
+var questions = [q1, q2, q3];
+var asked = [];
+
+
+// function to choose which question in the array will be displayed
+
+function chooseQuestion () {
+    var qNum = Math.floor(Math.random()* questions.length);
+    console.log(qNum);
+    var qChosen = questions[qNum];
+    console.log(qChosen);
+    asked.push(qChosen);
+    console.log(asked)
+}
+
+chooseQuestion ();
+
+// function to run the timer, on click stop timer
 
 // when play game is clicked, game begins
 
@@ -32,6 +45,7 @@ var q3 = {
 // begin timer counting down from 30 seconds
 
 // when player clicks on an answer stop the time and switch to the next screen which shows the correct answer and the image
+// if correct answer value equals answer of button clicked then correct
 
 // if time runs out before player clicks on an answer then switch to the next screen showing the correct answer and image
 
